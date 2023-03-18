adb09ce606f6005c92e1ee463e856d69ddb1374c
DardanB <>
2023-03-18 17:19:30 +0100

    data pre processing

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-from preprocess_names import preprocess_employee_names
+from preprocessing.preprocess_names import preprocess_employee_names
 
-# preprocess_employee_names("employee_working_hours.csv")
+preprocess_employee_names("employee_working_hours.csv")
