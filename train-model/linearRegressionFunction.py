7ddcbe9387f28e35346ed37aa1aa3a5303a74ad5
DardanB <>
2023-04-22 15:40:17 +0200

    linear regression function

--- train-model/linearRegressionFunction.py
+++ train-model/linearRegressionFunction.py
@@ -1,8 +1,7 @@
 import pandas as pd
-from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import LinearRegression
 
-
-def predict_hours_of_work_knn(employee_name, working_day, month, n_neighbors, weights):
+def predict_hours_of_work_lr(employee_name, working_day, month):
     # Read the dataset into a Pandas DataFrame
     df = pd.read_csv("final_preprocessed_employee_working_hours.csv")
 
@@ -10,8 +9,8 @@ def predict_hours_of_work_knn(employee_name, working_day, month, n_neighbors, we
     X = df[["working_day", "month"]]
     y = df["hours_of_work"]
 
-    # Create a KNN regression model
-    model = KNeighborsRegressor(n_neighbors=n_neighbors, weights=weights)
+    # Create a linear regression model
+    model = LinearRegression()
 
     # Train the model on the entire dataset
     model.fit(X, y)
@@ -23,4 +22,4 @@ def predict_hours_of_work_knn(employee_name, working_day, month, n_neighbors, we
     prediction = model.predict(user_input)
 
     # Return the predicted number of hours worked
-    return f"Employee {employee_name} is predicted to work {prediction[0]:.2f} hours using KNN."
+    return f"Employee {employee_name} is predicted to work {prediction[0]:.2f} hours using Linear Regression."
