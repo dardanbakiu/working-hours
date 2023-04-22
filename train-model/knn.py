2623febfbb7cbf2e1887b13de13b4fb40b125b2d
bajrams425 <bajrams425@gmail.com>
2023-04-22 15:22:40 +0200

    KNN model

---
+++ train-model/knn.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.neighbors import KNeighborsRegressor
+
+# Read the dataset into a Pandas DataFrame
+df = pd.read_csv("your_dataset.csv")
+
+# Extract the features (working_day and month) and the target (hours_of_work)
+X = df[["working_day", "month"]]
+y = df["hours_of_work"]
+
+# Create a KNN regression model
+model = KNeighborsRegressor()
+
+# Train the model on the entire dataset
+model.fit(X, y)
+
+# Get user input for the employee name, working day, and month
+employee_name = int(input("Enter employee name: "))
+working_day = int(input("Enter working day: "))
+month = int(input("Enter month: "))
+
+# Create a DataFrame with the user input
+user_input = pd.DataFrame({"working_day": [working_day], "month": [month]})
+
+# Use the model to make a prediction for the user input
+prediction = model.predict(user_input)
+
+# Print the predicted number of hours worked
+print(
+    f"Employee {employee_name} is predicted to work {prediction[0]:.2f} hours.")
