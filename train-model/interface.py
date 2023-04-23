f8e51ddbc0bee9920818d7d04c0877148eeede6e
bajrams425 <bajrams425@gmail.com>
2023-04-23 21:22:10 +0200

    SVF and minor interface change

--- train-model/interface.py
+++ train-model/interface.py
@@ -28,22 +28,24 @@ class Application(tk.Frame):
         self.algorithm_label = tk.Label(self, text="Algorithm:")
         self.algorithm_label.grid(row=1, column=0)
         self.algorithm_var = tk.StringVar(value="Linear Regression")
-        self.algorithm_dropdown = ttk.Combobox(self, textvariable=self.algorithm_var, values=["Linear Regression", "KNN", "Random Forest Regression"])
+        self.algorithm_dropdown = ttk.Combobox(self, textvariable=self.algorithm_var, values=[
+                                               "Linear Regression", "KNN", "Random Forest Regression"])
         self.algorithm_dropdown.grid(row=1, column=1)
 
         # Create the labels and entries for the algorithm parameters
-        self.param1_label = tk.Label(self, text="Parameter 1:")
+        self.param1_label = tk.Label(self, text="Week day:")
         self.param1_label.grid(row=2, column=0)
         self.param1_entry = tk.Entry(self)
         self.param1_entry.grid(row=2, column=1)
 
-        self.param2_label = tk.Label(self, text="Parameter 2:")
+        self.param2_label = tk.Label(self, text="Month:")
         self.param2_label.grid(row=3, column=0)
         self.param2_entry = tk.Entry(self)
         self.param2_entry.grid(row=3, column=1)
 
         # Create the predict button
-        self.predict_button = tk.Button(self, text="Predict", command=self.predict)
+        self.predict_button = tk.Button(
+            self, text="Predict", command=self.predict)
         self.predict_button.grid(row=4, column=1)
 
         # Create the result label
@@ -61,12 +63,14 @@ class Application(tk.Frame):
         if algorithm == "Linear Regression":
             result = predict_hours_of_work_lr(employee_name, param1, param2)
         elif algorithm == "KNN":
-            result = predict_hours_of_work_knn(employee_name, param1, param2, n_neighbors=5, weights="uniform")
+            result = predict_hours_of_work_knn(
+                employee_name, param1, param2, n_neighbors=5, weights="uniform")
         else:
             result = predict_hours_of_work_rfr(employee_name, param1, param2)
 
         # Print the predicted number of hours worked
         self.result_label.config(text=result)
+
 
 # Create the Tkinter GUI and start the main loop
 root = tk.Tk()
--- train-model/supportVectorFunction.py
+++ train-model/supportVectorFunction.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
+
 def predict_hours_of_work_rfr(employee_name, working_day, month):
     # Read the dataset into a Pandas DataFrame
     df = pd.read_csv("final_preprocessed_employee_working_hours.csv")
@@ -9,4 +10,18 @@ def predict_hours_of_work_rfr(employee_name, working_day, month):
     # Extract the features (working_day and month) and the target (hours_of_work)
     X = df[["working_day", "month"]]
     y = df["hours_of_work"]
+# Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=42)
 
+    # Create a Random Forest regression model
+    model = RandomForestRegressor(n_estimators=100, random_state=42)
+
+    # Train the model on the training set
+    model.fit(X_train, y_train)
+
+    # Use the model to make a prediction for the user input
+    prediction = model.predict([[working_day, month]])
+
+    # Return the predicted number of hours worked
+    return f"Employee {employee_name} is predicted to work {prediction[0]:.2f} hours using Random Forest Regression."
